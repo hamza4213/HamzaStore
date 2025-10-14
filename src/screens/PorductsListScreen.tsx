36c0c1e74ff793ec3988e7b8895f86c1f4bc4197
hamza4213 <hamzashabbir4213@gmail.com>
2025-10-15 00:11:56 +0500

    got rid of types and imports errors

--- src/screens/PorductsListScreen.tsx
+++ src/screens/PorductsListScreen.tsx
@@ -3,9 +3,9 @@ import { ViewStyle } from "react-native"
 import { Screen } from "@/components/Screen"
 import { Text } from "@/components/Text"
 import { useAppTheme } from "@/theme/context"
-import { useSafeAreaInsetsStyle } from "@/utils/useSafeAreaInsetsStyle"
-import { View } from "react-native-reanimated/lib/typescript/Animated"
 import { $styles } from "@/theme/styles"
+import { useSafeAreaInsetsStyle } from "@/utils/useSafeAreaInsetsStyle"
+import { FC } from "react"
 
 export const PorductsListScreen: FC = function PorductsListScreen() {
   const { themed, theme } = useAppTheme()
