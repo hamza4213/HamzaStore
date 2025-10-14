5b6492ebe6d55e64f79250c2c480ab7b5f969a22
hamza4213 <hamzashabbir4213@gmail.com>
2025-10-15 00:15:51 +0500

    not shwoing headers, will use our own

--- src/app/(tab)/home/_layout.tsx
+++ src/app/(tab)/home/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router"
 
 export default function HomeStackLayout() {
   return (
-    <Stack screenOptions={{ headerShown: true }}>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" options={{ title: "Products" }} />
       <Stack.Screen name="product-details" options={{ title: "Product Details" }} />
     </Stack>
